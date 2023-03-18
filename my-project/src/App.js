910dda59055bd0f9cbc11efd3cfc9c5abcc11778
choccy2003 <kujoxd@gmail.com>
2023-03-18 19:13:54 +0530

    new changes

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import Navbar from './components/Navbar';
 const App = ()=>{
  return(
     <div>
-        E-commerce
+        
+        <Navbar></Navbar>
     </div>
+
  )
 }
 export default App
---
+++ my-project/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../styles/navcss.css"
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='nav'>
+    <span className='item'>Home_icon</span>
+    <span className='item'>Search_box</span>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
